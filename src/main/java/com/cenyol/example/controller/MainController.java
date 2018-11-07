f25a5682f077911d71dcfec7e01b9fa18cf37f79
frankie <franklinyang2013@gmail.com>
2018-11-07 18:00:21 +0800

    ergergeg

--- src/main/java/com/cenyol/example/controller/MainController.java
+++ src/main/java/com/cenyol/example/controller/MainController.java
@@ -117,4 +117,8 @@ public class MainController {
         System.out.println("g2g34g34g hweig0239g09j23");
     }
 
+    public void hweig0239g09j2frankie1(){
+        System.out.println("g2g34g34g hweig0239g09j23");
+    }
+
 }
